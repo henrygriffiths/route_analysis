2ce8694f4a6f840a79774bbf0f11599c0f4258ca
Henry Griffiths <42880130+henrygriffiths@users.noreply.github.com>
2025-06-05 04:32:24 -0500

    feat: add counts

--- route_analysis.py
+++ route_analysis.py
@@ -4,9 +4,9 @@ import re
 
 
 columns = {'from': 'From', 'to': 'To'}
-directional = False
+directional = True
 
-with open('flights.csv', 'r') as csvfile:
+with open('example_flights.csv', 'r') as csvfile:
     flightdata = [{list(columns.keys())[list(columns.values()).index(k)]: v for k, v in r.items() if k in list(columns.values())} for r in csv.DictReader(csvfile, delimiter = ',', skipinitialspace = True)]
 
 
@@ -16,29 +16,37 @@ for i in range(len(flightdata)):
             flightdata[i][x] = re.search('(?<![A-Z0-9])([A-Z0-9]){3}(?![A-Z0-9])', flightdata[i][x]).group()
 
 
-routes = []
+routes = {}
 airports = {}
 
 for flight in flightdata:
     if f"{flight['from']}/{flight['to']}" not in routes:
-        routes.append(f"{flight['from']}/{flight['to']}")
+        routes[f"{flight['from']}/{flight['to']}"] = 0
+    routes[f"{flight['from']}/{flight['to']}"] += 1
     if not directional:
         if f"{flight['to']}/{flight['from']}" not in routes:
-            routes.append(f"{flight['to']}/{flight['from']}")
+            routes[f"{flight['to']}/{flight['from']}"] = 0
+        routes[f"{flight['to']}/{flight['from']}"] += 1
     
     if flight['from'] not in airports:
-        airports[flight['from']] = []
-    if flight['to'] not in airports[flight['from']]:
-        airports[flight['from']].append(flight['to'])
+        airports[flight['from']] = {'routes': {}, 'routes_count': 0, 'flights_count': 0}
+    if flight['to'] not in airports[flight['from']]['routes']:
+        airports[flight['from']]['routes'][flight['to']] = 0
+        airports[flight['from']]['routes_count'] += 1
+    airports[flight['from']]['routes'][flight['to']] += 1
+    airports[flight['from']]['flights_count'] += 1
     if not directional:
         if flight['to'] not in airports:
-            airports[flight['to']] = []
-        if flight['from'] not in airports[flight['to']]:
-            airports[flight['to']].append(flight['from'])
+            airports[flight['to']] = {'routes': {}, 'routes_count': 0, 'flights_count': 0}
+        if flight['from'] not in airports[flight['to']]['routes']:
+            airports[flight['to']]['routes'][flight['from']] = 0
+            airports[flight['to']]['routes_count'] += 1
+        airports[flight['to']]['routes'][flight['from']] += 1
+        airports[flight['to']]['flights_count'] += 1
 
 
-routes.sort()
-airports = {k: sorted(airports[k]) for k in sorted(airports) }
+routes = dict(sorted(routes.items()))
+airports = dict(sorted(airports.items()))
 
 
 while True:
@@ -48,12 +56,11 @@ while True:
     
     if '/' in uinput:
         if uinput in routes:
-            print(f"Found {uinput} in list of routes")
+            print(f"Found {uinput} in list of routes ({routes[uinput]})")
         else:
             print(f"Did not find {uinput} in list of routes")
     else:
         if uinput in airports:
-            print(f"Found {uinput} in list of airports")
-            print(f"Found routes from {uinput} to {', '.join(airports[uinput])}")
+            print(f"Found {airports[uinput]['routes_count']} routes from {uinput} to {', '.join([f"{airport} ({airport_count})" for airport, airport_count in airports[uinput]['routes'].items()])}")
         else:
             print(f"Did not find {uinput} in list of airports")
